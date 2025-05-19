572811cef1adf0fd4f689ac388366ee58c588ed5
milderz <mzunax@gmail.com>
2025-05-18 19:21:56 -0600

    feat: update Preview component to handle template selection and display products/employees

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -2,23 +2,55 @@ import '../components/styles/Preview.css';
 
 
 
-export default function Preview({ products }) {
-    return( <div className="preview-container" >
-        {products.map(product => (
+export default function Preview({ template }) {
+ 
+    if(!template){
+      return <div className="preview-container"><p>No template selected</p></div>
+    }
+
+    if(template.id === 1){
+      return( <div className="preview-container" >
+        
+        {template.products.map(product => (
           <article key={product.id} className="card">
             <img height="100px" width="100px" src={product.image} alt={product.name} className="product-image" />
             <header className="card-header">
             <p className='card-price'> ${product.price.toFixed(2)}</p>
             <h3 className='card-product-name'>{product.name}</h3>
-            <p className='product-rating'>{'★'.repeat(Math.floor(product.rating)) + '☆'.repeat(5 - Math.floor(product.rating))}<span className='product-rating-value'>({product.rating})</span></p>
             </header>
             <p className='card-product-description'>{product.description}</p>
             <button className="btn btn-primary">Add to Cart</button>
             <footer className="card-footer">
             
-
+  
             </footer>
           </article>
         ))}
       </div>)
+    }
+
+    if(template.id === 2){
+      return(  <div className="preview-container" >
+      
+        
+        {template.employees.map(product => (
+          <article key={product.id} className="card">
+            <img height="100px" width="100px" src={product.profileImage} alt={product.name} className="product-image" />
+            <header className="card-header">
+            {/* <p className='card-price'> ${product.price.toFixed(2)}</p> */}
+            <h3 className='card-product-name'>{product.name}</h3>
+            {/* <p className='product-rating'>{'★'.repeat(Math.floor(product.rating)) + '☆'.repeat(5 - Math.floor(product.rating))}<span className='product-rating-value'>({product.rating})</span></p> */}
+            </header>
+            <p className='card-product-description'>{product.description}</p>
+            <button className="btn btn-primary">Edit profile</button>
+            <footer className="card-footer">
+            
+  
+            </footer>
+          </article>
+        ))}
+      </div>)
+    }
+  
+    
 }
