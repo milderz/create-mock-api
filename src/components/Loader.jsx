55effb242cc21a1bc7164014822488073a4c66c2
milderz <mzunax@gmail.com>
2025-05-20 09:17:27 -0600

    fix: implement Loader component for improved loading indication

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+
+// import './Loader.css'; 
+// import loaderImage from '../../public/load-spinner.gif';
+
+
+const Loader = () => {
+    return (
+        <img height="20px" width="20px" className="loader" src="/load-spinner.gif" alt="Loading..." />
+    );
+};
+
+export default Loader;
