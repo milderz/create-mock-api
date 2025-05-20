ab4f5b499baaa9b8b31da48e0afb417c24cac47b
milderz <mzunax@gmail.com>
2025-05-20 15:15:28 -0600

    feat: enhance Button component to support link type and buttonStyle prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
 import './styles/Button.css';
 
-const Button = ({type, text, onClick, icon}) => {
-    return (<button className={ type === "primary" ? "primary-button" : "secondary-button" } onClick={onClick}>{icon}{text}</button>)
+const Button = ({buttonStyle, type, text, onClick, icon, linkTarget}) => {
+    if(type === 'button'){
+        return (
+            <button className={ buttonStyle === "primary" ? "primary-button" : "secondary-button" } onClick={onClick}>{icon}{text}</button>
+        )
+    }if(type === 'link'){
+        return (
+            <a className={ buttonStyle === "primary" ? "primary-button" : "secondary-button" } href={linkTarget}>{icon}{text}</a>
+        )
+    }
+    
 }
 
 export default Button;
