db46247a2bfbdc396688c066aba61c5297098418
milderz <mzunax@gmail.com>
2025-05-20 11:42:40 -0600

    fix: update icons in Header component for improved clarity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import Button from './Button';
 import './styles/Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFloppyDisk, faFilePen, faRightToBracket } from "@fortawesome/free-solid-svg-icons"
+import { faFloppyDisk, faTableColumns, faRightToBracket, faShareNodes } from "@fortawesome/free-solid-svg-icons"
 
 const Header = ({ currentPath, handleModalClose }) => {
 
@@ -14,8 +14,9 @@ const Header = ({ currentPath, handleModalClose }) => {
                     <h1>MockAPI Playground</h1>
                 </div>
                 <nav className="nav">
-                    {currentPath === "/create" ? (<Button type="secondary" text="Save API" icon={<FontAwesomeIcon icon={faFloppyDisk} />} onClick={handleModalClose}/>) : null}
-                    {currentPath === "/create" ? (<Button type="secondary" text="Select a template" icon={<FontAwesomeIcon icon={faFilePen} />} onClick={handleModalClose}/>) : null}
+                    {currentPath === "/create" ? (<Button type="secondary" text="My APIs" icon={<FontAwesomeIcon icon={faShareNodes} />} />) : null}
+                    {currentPath === "/create" ? (<Button type="secondary" text="Save API" icon={<FontAwesomeIcon icon={faFloppyDisk} />} />) : null}
+                    {currentPath === "/create" ? (<Button type="secondary" text="Select a template" icon={<FontAwesomeIcon icon={faTableColumns} />} onClick={handleModalClose}/>) : null}
 
                     <Button type="primary" text="Sign in" icon={<FontAwesomeIcon icon={faRightToBracket} />}/>
                 </nav>
