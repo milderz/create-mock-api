eff4136a4d9b7703df221e3aae3c69a50217018e
milderz <mzunax@gmail.com>
2025-05-20 15:15:34 -0600

    feat: integrate ClerkProvider in Header component for authentication support

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,30 +2,40 @@ import Button from './Button';
 import './styles/Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFloppyDisk, faTableColumns, faRightToBracket, faShareNodes } from "@fortawesome/free-solid-svg-icons"
+import {
+    ClerkProvider,
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    UserButton,
+} from "@clerk/clerk-react";
 
 const Header = ({ currentPath, handleModalClose }) => {
 
-    
+    const PUBLISHABLE_KEY = import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY
+
+
 
     return (
-        
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
             <header className="header" style={{ width: '90%', margin: 'auto' }}>
                 <div className="logo">
-                    <h1>MockAPI Playground</h1>
+                    <a className='logo-title' href="/">MockAPI Playground</a>
                 </div>
                 <nav className="nav">
-                    {currentPath === "/create" ? (<Button type="secondary" text="My APIs" icon={<FontAwesomeIcon icon={faShareNodes} />} />) : null}
-                    {currentPath === "/create" ? (<Button type="secondary" text="Save API" icon={<FontAwesomeIcon icon={faFloppyDisk} />} />) : null}
-                    {currentPath === "/create" ? (<Button type="secondary" text="Select a template" icon={<FontAwesomeIcon icon={faTableColumns} />} onClick={handleModalClose}/>) : null}
+                    {currentPath === "/create" ? (<Button type="button" buttonStyle="secondary" text="My APIs" icon={<FontAwesomeIcon icon={faShareNodes} />} />) : null}
+                    {currentPath === "/create" ? (<Button type="button" buttonStyle="secondary" text="Save API" icon={<FontAwesomeIcon icon={faFloppyDisk} />} />) : null}
+                    {currentPath === "/create" ? (<Button type="button" buttonStyle="secondary" text="Select a template" icon={<FontAwesomeIcon icon={faTableColumns} />} onClick={handleModalClose} />) : null}
 
-                    <Button type="primary" text="Sign in" icon={<FontAwesomeIcon icon={faRightToBracket} />}/>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                    <SignedOut>
+                    <Button type="link" buttonStyle="primary" text="Sign In" linkTarget="/create" icon={<FontAwesomeIcon icon={faRightToBracket} />} onClick={handleModalClose} />
+                    </SignedOut>
                 </nav>
             </header>
-        
-
-          
-      
-
+        </ClerkProvider>
     );
 };
 
