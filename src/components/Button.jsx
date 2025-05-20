7aed1f67941999c7ffbcc19f9b3a2767a7d4cdf2
milderz <mzunax@gmail.com>
2025-05-20 09:17:15 -0600

    fix: add icon prop to Button component for enhanced UI flexibility

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import './styles/Button.css';
 
-const Button = ({type, text, onClick}) => {
-    return (<button className={ type === "primary" ? "primary-button" : "secondary-button" } onClick={onClick}>{text}</button>)
+const Button = ({type, text, onClick, icon}) => {
+    return (<button className={ type === "primary" ? "primary-button" : "secondary-button" } onClick={onClick}>{icon}{text}</button>)
 }
 
 export default Button;
