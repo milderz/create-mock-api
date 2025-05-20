e789aa668fb07bc792a84c09fd82b43855bcf76d
milderz <mzunax@gmail.com>
2025-05-20 15:14:57 -0600

    fix: add Clerk integration and update Node adapter in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 
 export default defineConfig({
   // ...
-  integrations: [react()],
+  integrations: [react(),clerk()],
+  adapter: node({ mode: "standalone" }),
+  output: "server",
 });
