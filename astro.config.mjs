c38af6a4ab5c0a7c4aca75f1350704c25291ad3e
milderz <mzunax@gmail.com>
2025-05-20 18:16:56 -0600

    Add @astrojs/netlify dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import node from "@astrojs/node";
 import clerk from "@clerk/astro";
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
   // ...
   integrations: [react(),clerk()],
   adapter: node({ mode: "standalone" }),
   output: "server",
+  adapter: netlify(),
 });
